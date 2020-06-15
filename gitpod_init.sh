9e3003783155c21aaf4e8b2cba2d474f498937a3
tgodzik <tgodzik@virtuslab.com>
2020-06-15 17:48:12 +0200

    Update versions and make better use of cs install command

--- gitpod_init.sh
+++ gitpod_init.sh
@@ -1,16 +1,11 @@
  #!/bin/bash
 
 METALS_DIR="$GITPOD_REPO_ROOT/.metals"
-METALS_VERSION="0.8.3"
-BLOOP_VERSION="1.4.0-RC1-162-888454e1"
+METALS_VERSION="0.9.0"
+
+mkdir -p $METALS_DIR
 
 export PATH=$PATH:/usr/local/openjdk-8/bin
-
-curl -L https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py > ~/bloop_install.py
-python ~/bloop_install.py --dest $METALS_DIR/bloop_local
-curl -L https://piccolo.link/sbt-1.3.9.tgz > ~/sbt.tar.gz
-mkdir -p $METALS_DIR/sbt
-tar -xvf ~/sbt.tar.gz -C $METALS_DIR
 
 echo "-Dsbt.coursier.home=$METALS_DIR/coursier" >> .jvmopts
 echo "-Dcoursier.cache=$METALS_DIR/coursier" >> .jvmopts
@@ -19,11 +14,14 @@ echo "-sbt-boot $METALS_DIR/sbt/boot" >> .sbtopts
 echo "-ivy $METALS_DIR/.ivy2" >> .sbtopts
 
 curl -Lo $METALS_DIR/cs https://git.io/coursier-cli-linux && chmod +x $METALS_DIR/cs
-$METALS_DIR/cs fetch org.scalameta:metals_2.12:$METALS_VERSION --cache=$METALS_DIR/coursier 
-$METALS_DIR/cs fetch org.scalameta:scalafmt-cli_2.12:2.4.2 --cache=$METALS_DIR/coursier 
 
-source $METALS_DIR/bloop_local/bash/bloop
-alias sbt=$METALS_DIR/sbt/bin/sbt
-alias bloop=$METALS_DIR/bloop_local/bloop
+$METALS_DIR/cs launch org.scalameta:metals_2.12:$METALS_VERSION -- -J-Dcoursier.cache=./output -M scala.meta.metals.DownloadDependencies
+
+$METALS_DIR/cs install --install-dir $METALS_DIR --only-prebuilt=true bloop
+$METALS_DIR/cs install --install-dir $METALS_DIR sbt
+
+alias sbt=$METALS_DIR/sbt
+alias bloop=$METALS_DIR/bloop
+
 sbt -Dbloop.export-jar-classifiers=sources bloopInstall
-$METALS_DIR/cs launch ch.epfl.scala:bloopgun-core_2.12:$BLOOP_VERSION --cache=$METALS_DIR/coursier  -- compile --cascade root
+bloop compile --cascade root
